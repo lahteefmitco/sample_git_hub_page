0e53238868bab2889cc926acd23874272587764e
ABDUL LATHEEF <latheef.mitco@gmail.com>
2025-03-06 11:07:27 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blue,
         body: Center(
-          child: Text('Hello World 3'),
+          child: Container(
+            alignment: Alignment.center,
+            height: 200,
+            width: 300,
+            color: Colors.amber,
+            child: Padding(
+              padding: const EdgeInsets.all(26),
+              child: Text('Hello World 3'),
+            ),
+          ),
         ),
       ),
     );
